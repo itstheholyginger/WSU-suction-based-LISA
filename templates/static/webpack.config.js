c0597f13dfb7a72831d0b9ca692ed850875e8ce9
Amariah Del Mar <amariahdelmar@gmail.com>
2020-02-04 15:41:05 -0800

    starting on template

---
+++ templates/static/webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const resolve = require('path').resolve;
+const config = {
+    entry: __dirname + '/js/index.jsx',
+    output: {
+        path: resolve('../public'),
+        filename: 'bundle.js',
+        publicPath: resolve('../public')
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+};
+
+module.exports = config;
