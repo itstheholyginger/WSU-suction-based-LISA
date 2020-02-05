daea8ad083498836d073bec9c0b3f41af8fecc6d
Amariah Del Mar <amariahdelmar@gmail.com>
2020-02-04 18:58:46 -0800

    added webpack and react

--- templates/static/webpack.config.js
+++ templates/static/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const resolve = require('path').resolve;
 const config = {
+    devtool: 'eval-source-map',
     entry: __dirname + '/js/index.jsx',
     output: {
         path: resolve('../public'),
@@ -10,6 +11,18 @@ const config = {
     resolve: {
         extensions: ['.js', '.jsx', '.css']
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ["react", "es2015"]
+                }
+            }
+        ]
+    }
 };
 
 module.exports = config;
