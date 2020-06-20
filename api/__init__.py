f0166050c66e63b1fb19b407a15c43b2cc6209cb
Amariah Del Mar <amariahdelmar@gmail.com>
2020-06-19 20:12:41 -0700

    changed order of create app statements

--- api/__init__.py
+++ api/__init__.py
@@ -3,10 +3,10 @@ from flask_cors import CORS
 
 
 def create_app():
+    from .views import main
     app = Flask(__name__, static_folder='../build', static_url_path='/')
     app.config['DEBUG'] = True
     CORS(app)
 
-    from .views import main
     app.register_blueprint(main)
     return app
